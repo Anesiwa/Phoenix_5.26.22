d90d2be159a4ca44838e078b48d9945e047eedfa
Anesiwa <89821512+Anesiwa@users.noreply.github.com>
2022-05-26 21:39:18 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,12 +3,10 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
 import Home from "./Home";
-import Navigation from "./Navigation";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Navigation />
     <Home />
   </React.StrictMode>
 );
